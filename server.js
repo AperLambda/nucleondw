0bcdd8d4a48ee4f6bd0b01d45a5674f4cc12ee3a
LambdAurora <aurora42lambda@gmail.com>
2018-05-27 22:28:08 +0200

    commit?

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+const languages = require('src/languages')
 
 const {
     NUCLEONDW_PORT: port = 4200
@@ -10,11 +12,24 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', {})
+  res.render('index', { title: 'NucleonDW' })
 })
 
 app.get('/login/', (req, res) => {
-  res.render('login/login', {})
+  res.render('login/login', { title: 'NucleonDW - Login' })
+})
+
+app.use((req, res, next) => {
+  res.status(404)
+  // respond with html page
+  if (req.accepts('html'))
+    res.render('404', { title: '404', url: req.url })
+  // respond with json
+  else if (req.accepts('json'))
+    res.send({ error: 'Not found' });
+  else
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
 })
 
 app.listen(port, () => {
